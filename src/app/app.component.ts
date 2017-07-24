ef375c748d6a8cd407ea591882781247b0612594
Oren B <Analytics@atomi.local>
2017-07-24 17:35:43 +0300

    master detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,18 @@ export class Hero {
   id: number;
   name: string;
 }
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 @Component({
   selector: 'app-root',
@@ -13,17 +25,17 @@ export class Hero {
   styleUrls: ['./app.component.scss']
 })
 
-
-
 export class AppComponent implements OnInit {
   res: string;
   title = 'tour of Heros';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
 
   constructor(private backand: BackandService) { }
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+  }
 
   getList(): void {
     this.res = 'fetching objects...';
